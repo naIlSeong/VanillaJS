b4e76db65bb082db33b2bc58b6a3b3144b2c527f
Na Ilseong <nnis9857@gmail.com>
2020-08-16 06:05:05 +0900

    Greeting.js and Install SCSS

---
+++ js/greetings.js
@@ -0,0 +1,58 @@
+const form = document.querySelector(".js-greetingsForm");
+const input = form.querySelector("input");
+
+const USER_NAME = "name";
+
+function saveUser(text) {
+  localStorage.setItem(USER_NAME, text);
+}
+
+// If USER_NAME === null
+function askName() {
+  form.addEventListener("submit", handleSubmit);
+}
+
+// IF USER_NAME !== null
+function greetUser(userName) {
+  // Todo_1: Remove input
+  form.removeChild(input);
+  // Todo_2: Load name
+
+  // Todo_3: Display name
+  displayUser(userName);
+}
+
+function loadUser() {
+  const userName = localStorage.getItem(USER_NAME);
+  if (userName === null) {
+    askName();
+  } else {
+    greetUser(userName);
+  }
+}
+
+function displayUser(text) {
+  const div = document.createElement("div");
+  const greeting = document.createElement("span");
+  const name = document.createElement("span");
+
+  greeting.innerText = "Have a good day ";
+  name.innerText = text;
+
+  div.appendChild(greeting);
+  div.appendChild(name);
+  form.appendChild(div);
+}
+
+function handleSubmit() {
+  event.preventDefault();
+  const name = input.value;
+  saveUser(name);
+  greetUser(name);
+}
+
+function init() {
+  loadUser();
+}
+
+init();
